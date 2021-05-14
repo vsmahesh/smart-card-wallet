cf629904f09794581ca7067b303d8cd53c29434a
Mahesh V S <vs.mahesh@hotmail.com>
2021-05-14 13:11:06 +0530

    fix: Card list redirection issue
    Clicking on a health card item should redirect to the details page.

--- client/src/js/components/card-list-item.js
+++ client/src/js/components/card-list-item.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import { TagNames } from "./tagnames.js";
+import { ContextStore } from "../libs/context-store.js";
 
 class CardListItemComponent extends HTMLElement {
   constructor() {
@@ -10,6 +11,10 @@ class CardListItemComponent extends HTMLElement {
     const styles = ["card", "fluid"];
     this.classList.add(...styles);
     this.innerHTML = `<h5>${card.title}<small>Created On: ${card.createdOn}</small><small>Verified On: ${card.verifiedOn}</small></h5>`;
+    this.addEventListener("click", () => {
+      new ContextStore().setCard(card);
+      window.location.href = "/health-card.html";
+    });
   }
 }
 
