09ae56caed53cf526663ea8b91b348ccf0003ba8
Mahesh V S <vs.mahesh@hotmail.com>
2021-05-14 16:55:33 +0530

    fix: Verification not working
    Related modifications on the service worker

--- client/src/sw.js
+++ client/src/sw.js
@@ -1,5 +1,5 @@
 const CACHE_APP_SHELL_NAME = "health-card-app";
-const CACHE_DYNAMIC_NAME = "dynamic-v2";
+const CACHE_DYNAMIC_NAME = "v3";
 self.addEventListener("install", function (event) {
   console.log("[Service Worker] Installing Service Worker ...", event);
   event.waitUntil(
@@ -51,21 +51,29 @@ self.addEventListener("activate", function (event) {
 });
 
 self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request)
-        .catch((err) => {
-          console.log(err);
-        })
-        .then((serverResponse) => {
-          caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
-            cache.put(event.request.url, serverResponse.clone());
-            return serverResponse;
+  const destination = event.request.destination;
+  switch (destination) {
+    case "style":
+    case "script":
+    case "document":
+    case "image":
+      event.respondWith(
+        caches.open(CACHE_APP_SHELL_NAME).then((cache) => {
+          return cache.match(event.request).then((cachedResponse) => {
+            return (
+              cachedResponse ||
+              fetch(event.request).then((response) => {
+                cache.put(event.request, response.clone());
+                return response;
+              })
+            );
           });
-        });
-    })
-  );
+        })
+      );
+      break;
+
+    default:
+      event.respondWith(fetch(event.request));
+      break;
+  }
 });
